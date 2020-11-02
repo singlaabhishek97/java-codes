f12ab0e17a9b42bd457324808d22e3133ba27a07
Abhishek Singla <singla.abhishek97@gmail.com>
2020-11-02 20:10:22 +0530

    add insertnode in heap

--- DS Algorithms/Heap/MaxHeap.java
+++ DS Algorithms/Heap/MaxHeap.java
@@ -1,8 +1,12 @@
 public class MaxHeap {
     int[] heap;
     int heap_size;
+    int max_heap_size = 100;
     MaxHeap(int[] arr, int n) {
-        heap = arr;
+        heap = new int[max_heap_size];
+        for (int i = 0; i < n; i++) {
+            heap[i] = arr[i];
+        }
         heap_size = n;
         heapify(heap, n);
     }
@@ -10,6 +14,16 @@ public class MaxHeap {
         int n = heap_size - 1;
         for (int i = (n - 1) / 2; i >= 0; i--) {
             shift_down(heap, i, heap_size);
+        }
+    }
+    static void shift_up(int[] heap, int index) {
+        int p = (index - 1) / 2;
+        if(p < 0) return;
+        if(heap[p] < heap[index]) {
+            int temp = heap[p];
+            heap[p] = heap[index];
+            heap[index] = temp;
+            shift_up(heap, p);
         }
     }
     static void shift_down(int[] heap, int i, int heap_size) {
@@ -34,9 +48,20 @@ public class MaxHeap {
         }
     }
     void deleteNode(int index) {
+        if(index >= heap_size) {
+            return;
+        }
         heap[index] = heap[heap_size - 1];
         heap_size--;
         shift_down(heap, index, heap_size);
+    }
+    void insertNode(int val) {
+        if(heap_size == max_heap_size) {
+            return;
+        }
+        heap[heap_size] = val;
+        heap_size++;
+        shift_up(heap, heap_size - 1);
     }
     void displayHeap() {
         for(int i = 0; i < heap_size; i++) {
@@ -49,6 +74,8 @@ public class MaxHeap {
         MaxHeap myheap = new MaxHeap(arr, 6);
         myheap.displayHeap();
         myheap.deleteNode(1);
+        myheap.displayHeap();
+        myheap.insertNode(6);
         myheap.displayHeap();
     }
 }
