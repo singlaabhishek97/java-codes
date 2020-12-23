f076b92b968a3cd2216e347e9416ee3d1baf8efc
Abhishek Singla <singla.abhishek97@gmail.com>
2020-12-23 19:56:31 +0530

    add getter methods for cards and hand class

--- Design/src/Deck.java
+++ Design/src/Deck.java
@@ -11,8 +11,39 @@ class Card {
         this.faceValue = faceValue;
         this.suite = suite;
     }
+
+    //Getter methods for face and suite
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public Suit getSuite() {
+        return suite;
+    }
+
+    private boolean available = true;
+    public boolean isAvailable() {return available; }
+    public void markUnavailable() {available = false; }
+    public void markAvailable() {available = true; }
+
     public String toString() {
         return (faceValue + " of " + suite);
+    }
+}
+
+class Hand {
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    public int score() {
+        int score = 0;
+        for(Card card : cards) {
+            score += card.getFaceValue();
+        }
+        return score;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
     }
 }
 
